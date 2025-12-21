c7616a871e3d58169ca41d4248d47edaf4034904
Cristo Rodríguez <cristo.rodriguez.lara@gmail.com>
2025-12-21 13:24:48 +0100

    feat: make components dynamic using props

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import './App.css';
 import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el Curso de Intro a React.js', completed: false},
+  {text: 'Llorar con la Llorona', completed: false},
+  {text: 'LALALA', completed: false},
+];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter />
+      <TodoCounter completed={16} total={25}/>
       <TodoSearch />
-      
+    
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem
+          key = {todo.text}
+          text = {todo.text}
+          completed = {todo.completed}
+          />
+        ))}
       </TodoList>
 
-      {/* <CreateTodoButton /> */}
-    </div>
+      <CreateTodoButton/>
+
+    </React.Fragment>
   );
 }
 
